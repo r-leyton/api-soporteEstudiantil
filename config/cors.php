c0b2017014e01d26bbd4ac207301b64d46638703
PieroL1 <198247247+PieroL1@users.noreply.github.com>
2025-12-22 13:10:56 -0500

    fix: nuevo cors

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:4321', 'http://127.0.0.1:4321','https://incadev.vercel.app'],
+    'allowed_origins' => ['http://localhost:4321', 'http://127.0.0.1:4321', 'https://incadev.vercel.app', 'http://136.110.152.168'],
 
     'allowed_headers' => ['*'],
 
